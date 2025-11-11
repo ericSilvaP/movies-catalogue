087b5519b348caa5a7cf36e03d4abc8bf9eac53f
Eric Silva <ericsilvap13@gmail.com>
2025-11-10 21:48:45 -0300

    rem: remove getMovie loose function

--- scripts/tmdb.js
+++ scripts/tmdb.js
@@ -30,32 +30,3 @@ export class TMBd {
     }
   }
 }
-
-export async function getMovie(
-  movieId,
-  apiKey,
-  appendToResponse = [],
-  language = 'pt-BR'
-) {
-  let url = `https://api.themoviedb.org/3/movie/${movieId}?language=${language}`
-  if (appendToResponse.length > 0) {
-    url += `&append_to_response=${appendToResponse.join(',')}`
-  }
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    },
-  }
-
-  try {
-    const response = await fetch(url, options)
-    if (!response.ok)
-      throw new Error(`Falha ao buscar o filme (status: ${response.status})`)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    throw error
-  }
-}
