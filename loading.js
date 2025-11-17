d66e1a49bf78db1a784d9f7725f6535dcb527e49
Eric Silva <ericsilvap13@gmail.com>
2025-11-16 22:13:04 -0300

    feat: create person detail page with js

--- loading.js
+++
@@ -1,7 +0,0 @@
-export function showLoading() {
-  document.getElementById('loading').classList.remove('hidden')
-}
-
-export function hideLoading() {
-  document.getElementById('loading').classList.add('hidden')
-}
